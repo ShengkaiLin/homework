85eefb298ad8c8d4bd5ca06d060d83609eaf1dcc
DESKTOP-83TTEVF\ShengKaiLin <southskylife@gmail.com>
2023-02-01 23:19:12 +0800

    提交火車票買賣作業2 連續購買車票

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -8,14 +8,23 @@ public class Tester {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How many tickets do you want to buy:");
-        int totalTickets = scanner.nextInt();
 
-        System.out.println("How many round-trip tickets do you want to buy?");
-        int roundTripTickets = scanner.nextInt();
+        int endBuyFlowCode = 0;
 
-        System.out.println("The following is your ticket details:");
-        Conductor conductor = new Conductor(totalTickets,roundTripTickets);
-        conductor.showTransactionInfo();
+        while (endBuyFlowCode != -1) {
+            System.out.println("How many tickets do you want to buy:");
+            int totalTickets = scanner.nextInt();
+
+            System.out.println("How many round-trip tickets do you want to buy?");
+            int roundTripTickets = scanner.nextInt();
+
+            System.out.println("The following is your ticket details:");
+            Conductor conductor = new Conductor(totalTickets, roundTripTickets);
+            conductor.showTransactionInfo();
+
+            System.out.println("If you want to stop buying ticket , you can input number -1 or not:");
+            endBuyFlowCode = scanner.nextInt();
+        }
+
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -1,15 +1,24 @@
 package com.train
 
 fun main(args: Array<String>) {
-    println("How many tickets do you want to buy:")
-    val totalTicket = readLine()?.toInt()
 
-    println("How many round-trip tickets do you want to buy?")
-    val roundTripTickets = readLine()?.toInt()
+    var endBuyFlowCode: Int? = 0
 
-    println("The following is your ticket details:")
-    val conKotlin = ConductorKotlin(totalTicket, roundTripTickets)
-    conKotlin.showTransactionDetail()
+    while (endBuyFlowCode != -1) {
+        println("How many tickets do you want to buy:")
+        val totalTicket = readLine()?.toInt()
+
+        println("How many round-trip tickets do you want to buy?")
+        val roundTripTickets = readLine()?.toInt()
+
+        println("The following is your ticket details:")
+        val conKotlin = ConductorKotlin(totalTicket, roundTripTickets)
+        conKotlin.showTransactionDetail()
+
+        println("If you want to stop buying ticket , you can input number -1 or not:")
+        endBuyFlowCode = readLine()?.toInt()
+    }
+
 }
 
 class ConductorKotlin(var totalTicket: Int?, var roundTripTicket: Int?, var singlePrice: Int = 1000) {
